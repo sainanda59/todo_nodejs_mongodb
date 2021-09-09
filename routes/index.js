01fb0af29e3eeb04c0e07d0642cdc9469d8cce5d
Sai Nanda <saisoumyaknanda@gmail.com>
2021-09-09 22:13:59 +0530

    update

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const homeContoller  = require('../controllers/home_controller');
-
-const router = express.Router();
-
-router.get('/',homeContoller.home);
-
-module.exports = router;
